47a612fddf6faf63ce126b369483319406307660
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-07-12 23:17:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 //complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-		
-	}
-	get name(){
-			return this.name
-		}
-	set age(newAge){
-		this.age=newAge;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    this._age = value;
+  }
 }
+
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name) is studying`);
-	}
+		console.log(`${this.name} is studying`);
+     }
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name) is studying`);
+		console.log(`${this.name} is teaching`);
 	}
 }
-const person=new Person("John",25);
-const person=new Person("John",30);
-const student =new Student("John",30);
-student.study();
-const student =new Student("Alice",30);
-student.study();
-const teacher =new Teacher("John",30);
-teacher.teach();
-const teacher =new Teacher("Alice",30);
-teacher.teach();
+const person = new Person("John", 25);
+person.age = 30;
+console.log(person.age);  
 
+const student = new Student("John", 30);
+student.study();  
+
+const teacher = new Teacher("Alice", 30);
+teacher.teach();  
 
 // Do not change the code below this line
 window.Person = Person;
